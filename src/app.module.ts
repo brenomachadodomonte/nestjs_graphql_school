47a353dcc5a6902cf4a1b90536c4ac4322bc2b0c
brenomachadodomonte <brenomachadodomonte@gmail.com>
2021-11-02 09:04:00 -0300

    Creating Lesson Resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile: true
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
     }),
     LessonModule
   ],
