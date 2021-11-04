17f0a783951eb9aa68984add9c73dcfd71136099
brenomachadodomonte <brenomachadodomonte@gmail.com>
2021-11-04 20:35:00 -0300

    Creating Lesson Service

--- src/lesson/lesson.module.ts
+++ src/lesson/lesson.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LessonResolver } from './lesson.resolver';
+import { LessonService } from './lesson.service';
 
 @Module({
     providers: [
-        LessonResolver
+        LessonResolver,
+        LessonService
     ]
 })
 export class LessonModule {}
