79932f453cad85b942e681e28c35090dcf8cfaab
brenomachadodomonte <brenomachadodomonte@gmail.com>
2021-11-05 14:30:00 -0300

    Installing uuid lib and creating create lesson function

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LessonRepository } from './lesson.repository';
 
 @Injectable()
-export class LessonService {}
+export class LessonService {
+
+    constructor(
+        @InjectRepository(LessonRepository)
+        private repository: LessonRepository
+    ){}
+
+    createLesson(name: string, startDate: string, endDate: string) {
+        const lesson = this.repository.create({
+            name, startDate, endDate
+        });
+
+        
+    }
+}
