3842705f2ac03a7f76f0738afa8567dac2527dad
brenomachadodomonte <brenomachadodomonte@gmail.com>
2021-11-11 14:30:00 -0300

    Creating createStudent function in Service

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Student } from './student.entity';
+import { CreateStudentInput } from './student.input';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class StudentService {
@@ -11,4 +13,16 @@ export class StudentService {
         private repository: Repository<Student>
     ){}
 
+
+    createStudent(createStudentInput: CreateStudentInput): Promise<Student> {
+        const { firstName, lastName } = createStudentInput;
+
+        const student = this.repository.create({
+            id: uuid(),
+            firstName,
+            lastName
+        })
+
+        return this.repository.save(student);
+    }
 }
