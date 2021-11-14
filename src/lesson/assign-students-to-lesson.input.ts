eff4e69bd32a5d0aff9564b18ae5a9ae971d1fb3
brenomachadodomonte <brenomachadodomonte@gmail.com>
2021-11-14 14:30:00 -0300

    Adding validation to Assign Students to lesson input

--- src/lesson/assign-students-to-lesson.input.ts
+++ src/lesson/assign-students-to-lesson.input.ts
@@ -1,12 +1,16 @@
 import { Field, ID, InputType } from "@nestjs/graphql";
+import { IsUUID } from "class-validator";
 
 
 @InputType()
 export class AssignStudentsToLessonInput {
 
+    @IsUUID()
     @Field(type => ID)
     lessonId: string;
 
+    @IsUUID("4", { each: true })
     @Field(type => [ID])
     studentsId: string[];
+    
 }
